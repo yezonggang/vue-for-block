3f8a286acce23494e0a037b69168513b15a82e15
叶子叶 <717818895@qq.com>
2023-02-22 10:00:43 +0800

    js-cookie设置过期时间
    js-cookie设置过期时间

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,12 +2,15 @@ import Cookies from 'js-cookie'
 
 const TokenKey = 'Authorization'
 
+let seconds = 3600
+
+let expires = new Date(new Date() * 1 + seconds * 1000)
 export function getToken() {
   return Cookies.get(TokenKey)
 }
-
+// token 3600s 过期
 export function setToken(token) {
-  return Cookies.set(TokenKey, token)
+  return Cookies.set(TokenKey, token, { expires: expires })
 }
 
 export function removeToken() {
